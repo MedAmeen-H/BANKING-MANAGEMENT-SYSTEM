c03c3e3e569f9f5d5872424c32694fddd6ec509f
XXIII-VI <haddejiadem123@gmail.com>
2025-11-29 19:06:20 +0100

    Update DoublyLinkedListLoansMeth.h

--- Headers/Methods/DoublyLinkedListLoansMeth.h
+++ Headers/Methods/DoublyLinkedListLoansMeth.h
@@ -2,19 +2,20 @@
 #ifndef DOUBLYLINKEDLISTLOANSMETH_H
 #define DOUBLYLINKEDLISTLOANSMETH_H
 #include "DoublyLinkedListLoans.h"
-NodeLoan* createNodeLoan(StructLoan data);
+NodeLoan* createNodeLoan(Loan data);
 void destroyNodeLoan(NodeLoan* node);
 ListLoan createListLoan();
 void destroyListLoan(ListLoan* L);
 bool ListLoanIsEmpty(const ListLoan& L);
 bool ListLoanIsFull(const ListLoan& L);
 int ListLoanSize(const ListLoan& L);
-int insertLoan(ListLoan* L, StructLoan e, int pos);
+int insertLoan(ListLoan* L, Loan e, int pos);
 int removeAtLoan(ListLoan* L, int pos);
 int getLoan(const ListLoan& L, int pos);
 void displayListLoan(const ListLoan& L);
 ListLoan CopyListLoan(const ListLoan& L);
 bool CompareLoanLists(const ListLoan& L1, const ListLoan& L2);
 #endif
+
 
 
