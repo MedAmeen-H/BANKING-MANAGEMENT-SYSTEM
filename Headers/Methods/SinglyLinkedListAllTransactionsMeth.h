e3fa0b8199b9018a3edf588b7694d839a04a868b
XXIII-VI <haddejiadem123@gmail.com>
2025-11-29 19:07:01 +0100

    Update SinglyLinkedListAllTransactionsMeth.h

--- Headers/Methods/SinglyLinkedListAllTransactionsMeth.h
+++ Headers/Methods/SinglyLinkedListAllTransactionsMeth.h
@@ -2,19 +2,20 @@
 #ifndef SINGLYLINKEDLISTALLTRANSACTIONS_METH_H
 #define SINGLYLINKEDLISTALLTRANSACTIONS_METH_H
 #include "SinglyLinkedListAllTransactions.h"
-NodeAllTransactions* createNodeAllTransactions(StructTransaction value);
+NodeAllTransactions* createNodeAllTransactions(Transaction value);
 void destroyNodeAllTransactions(NodeAllTransactions* node);
 ListAllTransactions createListAllTransactions();
 void destroyListAllTransactions(ListAllTransactions* L);
 bool ListAllTransactionsisEmpty(const ListAllTransactions& L);
 bool ListAllTransactionsisFull(const ListAllTransactions& L);
 int ListAllTransactionsSize(const ListAllTransactions& L);
-int InsertAllTransaction(ListAllTransactions* L, StructTransaction e, int pos);
+int InsertAllTransaction(ListAllTransactions* L, Transaction e, int pos);
 int removeAllTransactionAt(ListAllTransactions* L, int pos);
-StructTransaction getAllTransaction(const ListAllTransactions& L, int pos);
+Transaction getAllTransaction(const ListAllTransactions& L, int pos);
 void displayListAllTransactions(const ListAllTransactions& L);
 ListAllTransactions CopyListAllTransactions(const ListAllTransactions& L);
 bool CompareListAllTransactions(const ListAllTransactions& L1, const ListAllTransactions& L2);
 
 
 #endif
+
