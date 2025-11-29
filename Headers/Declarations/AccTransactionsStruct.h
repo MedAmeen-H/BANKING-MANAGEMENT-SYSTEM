dbc6e46727dc911dbb19a8ab4fc53c19d72868d6
ahmedsinda <ahmed.sinda06@gmail.com>
2025-11-29 21:24:13 +0100

    Add AccTransactionsStack structure definition

---
+++ Headers/Declarations/AccTransactionsStruct.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef ACCTRANSACTIONSSTACK_H
+#define ACCTRANSACTIONSSTACK_H
+#include "StructTransaction.h"
+constexpr int Max = 100;
+struct AccTransactionsStack {
+	StructTransaction elements[Max];
+	int Top;
+};
+#endif
