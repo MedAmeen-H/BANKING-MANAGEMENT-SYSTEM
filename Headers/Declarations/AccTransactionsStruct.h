506fe22957df873056bfe3c36ceb36bd4a62780c
MedAmeen-H <medameenh@gmail.com>
2025-12-07 23:09:12 +0100

    Delete Headers directory

--- Headers/Declarations/AccTransactionsStruct.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACCTRANSACTIONSSTACK_H
-#define ACCTRANSACTIONSSTACK_H
-#include "StructTransaction.h"
-constexpr int Max = 100;
-struct AccTransactionsStack {
-	StructTransaction elements[Max];
-	int Top;
-};
-#endif
